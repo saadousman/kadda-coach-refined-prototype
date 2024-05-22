7fa8782d892808d32f85c6f1602f747c075a9004
saadousman <tuan@likemindsconsulting.com>
2024-05-22 13:10:33 +0530

    sss

--- scripts.js
+++ scripts.js
@@ -3,3 +3,14 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('points').innerText = '1500';
     document.getElementById('username').innerText = 'JohnDoe';
 });
+
+function startGame(gameTitle) {
+    document.getElementById('main-screen').classList.add('d-none');
+    document.getElementById('game-screen').classList.remove('d-none');
+    document.getElementById('game-title').innerText = gameTitle;
+}
+
+function goBackToMain() {
+    document.getElementById('main-screen').classList.remove('d-none');
+    document.getElementById('game-screen').classList.add('d-none');
+}
